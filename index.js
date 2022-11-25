00e2110aab7cb191582cc7c06aa0ffa4ab051cba
nytswa <dark.nitsua@gmail.com>
2022-11-25 09:28:09 -0300

    3.22: Lint configuration
    Final 3 exercise

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 require('dotenv').config()
 
 // Solo se requiere 1 sola ejecución para la conección al servidor.
@@ -35,122 +36,122 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :c
 
 // Base URL
 app.get('/', (req, res) => {
-    res.send('<h1>Phonebook Backend Final Home Page</h1>')
+  res.send('<h1>Phonebook Backend Final Home Page</h1>')
 })
 
 // INFO page using MongoDB
 app.get('/info', (req, res, next) => {
-    const date = new Date() 
-    Person.find({}).then(contacts => {
-        res.send(`
+  const date = new Date()
+  Person.find({}).then(contacts => {
+    res.send(`
             <p>Phonebook has info for ${contacts.length} people</p>
             <p>${date}</p>
         `)
-    }).catch(err => next(err))
+  }).catch(err => next(err))
 })
 
 // Get all using MongoDB
 app.get('/api/persons', (req, res) => {
-    Person.find({})
-        .then(contacts => res.json(contacts))
+  Person.find({})
+    .then(contacts => res.json(contacts))
 })
 
 // Get 1 with id using MongoDB
 app.get('/api/persons/:id', (req, res, next) => {
-    const { id } = req.params
-    console.log(typeof id)
-    Person.findById(id).then(person => {
-        if (person) {
-            res.json(person)
-        } else {
-            res.status(404).end()
-        }
-    }).catch(err => next(err))
-        // sending to last USE: middleware to catch not Founds and 40- 40x errors
+  const { id } = req.params
+  console.log(typeof id)
+  Person.findById(id).then(person => {
+    if (person) {
+      res.json(person)
+    } else {
+      res.status(404).end()
+    }
+  }).catch(err => next(err))
+  // sending to last USE: middleware to catch not Founds and 40- 40x errors
 })
 
 // Delete 1 using MongoDB
 app.delete('/api/persons/:id', (req, res, next) => {
-    const { id } = req.params
+  const { id } = req.params
 
-    // Search and Destroy - findByIdAndDelete
-    Person.findByIdAndRemove(id)
-        .then(() => res.status(204).end())
-        .catch(err => next(err))
+  // Search and Destroy - findByIdAndDelete
+  Person.findByIdAndRemove(id)
+    .then(() => res.status(204).end())
+    .catch(err => next(err))
 })
 
 // Update 1 using MongoDB
 app.put('/api/persons/:id', (req, res, next) => {
-    const { id } = req.params
-    const updatedContactData = req.body
+  const { id } = req.params
+  const updatedContactData = req.body
 
-    const contact = {
-        name: updatedContactData.name,
-        number: updatedContactData.number
-    }
+  const contact = {
+    name: updatedContactData.name,
+    number: updatedContactData.number
+  }
 
-    // result will be the findById result (not the updated) by default
-    Person.findByIdAndUpdate(id, contact, { new: true }).then(result => {
-        res.status(200).json(result)
-    }).catch(err => next(err))
+  // result will be the findById result (not the updated) by default
+  Person.findByIdAndUpdate(id, contact, { new: true }).then(result => {
+    res.status(200).json(result)
+  }).catch(err => next(err))
 })
 
-// POST using MongoDB 
+// POST using MongoDB
 app.post('/api/persons', (req, res, next) => {
-    const newData = req.body
+  const newData = req.body
 
-    // No input Control
-    if (!newData) {
-        res.status(400).json({
-            error: 'Unexpected error: No contact data'
-        })
-    } else if (!newData.name || !newData.number) {
-        res.status(400).json({
-            error: 'Name or Number is missing'
-        })
-    } else if (newData.name.length < 3) {
-        res.status(400).json({
-            error: 'Name must be at least 3 characters long'
-        })
-    } else if (newData.number.length < 8) {
-        res.status(400).json({
-            error: 'Number must be at least 8 characters long'
-        })    
-    } else if (!(phoneValidation(newData.number.toString()))) {
-        res.status(400).json({
-            error: 'Phone must be a valid phone number'
-        })
-    }
-    //
+  // No input Control
+  if (!newData) {
+    res.status(400).json({
+      error: 'Unexpected error: No contact data'
+    })
+  } else if (!newData.name || !newData.number) {
+    res.status(400).json({
+      error: 'Name or Number is missing'
+    })
+  } else if (newData.name.length < 3) {
+    res.status(400).json({
+      error: 'Name must be at least 3 characters long'
+    })
+  } else if (newData.number.length < 8) {
+    res.status(400).json({
+      error: 'Number must be at least 8 characters long'
+    })
+  } else if (!(phoneValidation(newData.number.toString()))) {
+    res.status(400).json({
+      error: 'Phone must be a valid phone number'
+    })
+  }
+  //
 
-    // Already exists (update this with mongo database find)
-    // const doesNotExist = persons.findIndex(p => p.name === person.name)
-    
-    // Trying find with mongo
-    else {
-        Person.exists({ name: newData.name }).then(contactFind => {
-            if (contactFind) {
-                console.log('Person already Exists', contactFind)
-                res.status(400).json({
-                    error: 'Name must be unique'
-                })
-            } else {
-                // Add
-                // Create ID: Not anymore since MongoDB creates its own IDs
-                
-                // Create new Person/Contact
-                const contact = new Person({
-                    name: newData.name,
-                    number: newData.number
-                })
-            
-                // Save Person/Contact
-                contact.save().then(savedContact => {
-                    res.status(201).json(savedContact)
-                })
-            }
-        }).catch(err => next(err))
-    }
+  // Already exists (update this with mongo database find)
+  // const doesNotExist = persons.findIndex(p => p.name === person.name)
+
+  // Trying find with mongo
+  else {
+    Person.exists({ name: newData.name }).then(contactFind => {
+      if (contactFind) {
+        console.log('Person already Exists', contactFind)
+        res.status(400).json({
+          error: 'Name must be unique'
+        })
+      } else {
+        // Add
+        // Create ID: Not anymore since MongoDB creates its own IDs
+
+        // Create new Person/Contact
+        const contact = new Person({
+          name: newData.name,
+          number: newData.number
+        })
+
+        // Save Person/Contact
+        contact.save().then(savedContact => {
+          res.status(201).json(savedContact)
+        })
+      }
+    }).catch(err => next(err))
+  }
 })
 
 
@@ -159,7 +160,8 @@ app.use(notFound)  // 404 NOT FOUND
 app.use(handleErrors)  // Catch Errors
 
 
+// eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Server Running on port ${PORT}`)
+  console.log(`Server Running on port ${PORT}`)
 })
